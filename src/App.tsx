75b807f5d1db08fc8310ce632c81b9c5809049a9
yogesh-1413 <yogeshwaraododda@gmail.com>
2026-03-19 22:01:16 +0530

    Just started a React App and installed a few dependices.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <div className="bg-yellow-200 p-4"> 
+    
+      <p className="text-pink-200 font-bold">Hello AI</p>
+    </div>
+  )
+}
